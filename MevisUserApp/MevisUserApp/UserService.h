ef600e8e762bf9c7a7e7c8f6f16cbdd083307939
TimofeyZykov <zikovtm@gmail.com>
2020-08-01 14:54:10 +0300

    feat: add UserService class

---
+++ MevisUserApp/MevisUserApp/UserService.h
@@ -0,0 +1,17 @@
+//
+//  UserService.h
+//  MevisUserApp
+//
+//  Created by Tsimafei Zykau on 8/1/20.
+//  Copyright © 2020 Timofey Zykov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MevisUserApp/MevisUserApp/UserService.m
@@ -0,0 +1,13 @@
+//
+//  UserService.m
+//  MevisUserApp
+//
+//  Created by Tsimafei Zykau on 8/1/20.
+//  Copyright © 2020 Timofey Zykov. All rights reserved.
+//
+
+#import "UserService.h"
+
+@implementation UserService
+
+@end
